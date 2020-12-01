8e9e9d22550ecd2e120d140a2218e1da4191f02e
diramtz <nancy.dira@gmail.com>
2020-12-01 15:14:27 -0600

    primera parte de limpieza

---
+++ 02-clean.R
@@ -0,0 +1,20 @@
+
+clean_colnames <- function(colnames){
+  temp <- gsub("(?<=[A-Za-z])(?=[A-Z])", "_", colnames, perl = TRUE)
+  tolower(temp)
+}
+
+replace_NULLs <- function(column){
+  na_if(column, 'NULL')
+}
+
+data_type_conv <- function(walmart){
+  walmart$trip_type <- as.factor(walmart$trip_type)
+  walmart$visit_number <- as.numeric(walmart$visit_number)
+  walmart$weekday <- as.factor(walmart$weekday)
+  walmart$upc <- as.numeric(walmart$upc)
+  walmart$scan_count <- as.numeric(walmart$scan_count)
+  walmart$department_description <- as.factor(walmart$department_description)
+  walmart$fineline_number <- as.numeric(walmart$fineline_number)
+  walmart
+}
