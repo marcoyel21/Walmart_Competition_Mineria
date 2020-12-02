fef3e6621f75ec7e056d9e1e06d6e2fbc024bfd1
santibatte <santiagobattezzati@gmail.com>
2020-12-02 19:20:07 -0300

    agrega cambios para hacer feather desde R y archivo walmart.feather y transforma filename a factor en 02-clean

--- 02-clean.R
+++ 02-clean.R
@@ -8,6 +8,7 @@ replace_NULLs <- function(column){
   na_if(column, 'NULL')
 }
 
+
 data_type_conv <- function(walmart){
   walmart$trip_type <- as.factor(walmart$trip_type)
   walmart$visit_number <- as.numeric(walmart$visit_number)
@@ -15,6 +16,6 @@ data_type_conv <- function(walmart){
   walmart$upc <- as.numeric(walmart$upc)
   walmart$scan_count <- as.numeric(walmart$scan_count)
   walmart$department_description <- as.factor(walmart$department_description)
-  walmart$fineline_number <- as.numeric(walmart$fineline_number)
+  walmart$fineline_number <- as.factor(walmart$fineline_number)
   walmart
 }
